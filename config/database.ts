c0cec3a3043658265dc134ad0383acb0a759aff0
Aman Panwar <aman@bytive.in>
2025-02-27 13:09:35 +0530

    fix(database): fix the db connection

--- config/database.ts
+++ config/database.ts
@@ -14,17 +14,17 @@ export default ({ env }) => {
         ssl: false
       }
     },
-    // postgres: {
-    //   connection: {
-    //     connectionString: env('DATABASE_URL'),
-    //     host: env('DATABASE_HOST', 'localhost'),
-    //     port: env.int('DATABASE_PORT', 5432),
-    //     database: env('DATABASE_NAME', 'strapi'),
-    //     user: env('DATABASE_USERNAME', 'strapi'),
-    //     password: env('DATABASE_PASSWORD', 'strapi'),
-    //     ssl: false
-    //   }
-    // },
+    postgres: {
+      connection: {
+        connectionString: env('DATABASE_URL'),
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'strapi'),
+        user: env('DATABASE_USERNAME', 'strapi'),
+        password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: false
+      }
+    },
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', 'db/data.db')),
